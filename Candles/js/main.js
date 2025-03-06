2f2ccd92544990e6ca9e3d3feecdf756d68b1587
ZaneZeltina <zanezeltina32@inbox.lv>
2025-03-06 16:39:40 +0200

    Create_Web_Page_Candles, day_2, part_2

--- Candles/js/main.js
+++ Candles/js/main.js
@@ -4,3 +4,31 @@ const menuMobile = document.querySelector('.header-menu__list');
 menuBtn.addEventListener('click', () => {
   menuMobile.classList.toggle('header-menu__list--active');
 });
+
+const swiperOne = new Swiper(".feedback__slider", {
+  loop: true,
+  slidesPerView: 1,
+  speed: 6500,
+  freeMode: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperTwo = new Swiper(".certificates__slider", {
+  loop: true,
+  slidesPerView: 3,
+  speed: 6500,
+  freeMode: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
